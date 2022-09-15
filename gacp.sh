7cae17c260484bc32299f458bb1faf03ad942d9e
Peter Krantz <zyrree@gmail.com>
2022-09-15 21:26:42 +0200

    parsed output of git status --porcelain, can choose files with gum

--- gacp.sh
+++ gacp.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+declare -a dirty untracked deleted newfile copied renamed
+git status --porcelain | (
+    while read line ; do
+        case "${line//[[:space:]]/}" in
+        'M'*)          dirty+=(${line:2}) ; ;;
+        'UU'*)         dirty+=(${line:2}) ; ;;
+        'D'*)          deleted+=(${line:2}) ; ;;
+        '??'*)         untracked+=(${line:2}) ; ;;
+        'A'*)          newfile+=(${line:2}) ; ;;
+        'C'*)          copied+=(${line:2}) ; ;;
+        'R'*)          renamed+=(${line:2}) ; ;;
+        esac
+    done
+    gum choose --no-limit "${dirty[@]}"
+)
