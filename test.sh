9f155341f414f5237d99eefef092f4070f666c5e
Peter Krantz <zyrree@gmail.com>
2023-05-18 12:23:24 +0200

    test

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if 'gum' command is available
+if ! command -v gum &> /dev/null
+then
+    echo "'gum' command could not be found. Please install it and try again."
+    exit
+fi
+
+# Check what changes have happened in the current git directory
+CHANGES=$(git status --porcelain)
+echo "$CHANGES"
+
+# Present a list of the potential files to be added
+FILES_TO_ADD=$(echo "$CHANGES" | awk '{print $2}')
+CHOSEN_FILES=$(gum choose $FILES_TO_ADD)
+
+# Git add the files selected by the user
+git add $CHOSEN_FILES
+
+# Ask user for commit message
+COMMIT_MSG=$(gum input --placeholder "Enter commit message")
+
+# Commit the changes
+git commit -m "$COMMIT_MSG"
+
+# Ask the user if they want to push the changes, and if confirmed, push the changes
+gum confirm "Push changes?" && git push
+
